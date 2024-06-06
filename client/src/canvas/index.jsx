f5ee333b708016276e5bfb0a3f258c95496dc81b
Kaveesha Dissanayake <90174106+KaveeshaDisssanayake@users.noreply.github.com>
2024-06-06 13:12:44 +0530

    ThreeJS Camera

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -13,7 +13,7 @@ const CanvasModel = () => {
       <Environment preset='city'/>
 
       <CameraRig>
-        {/*<BackDrop/>*/}
+        <BackDrop/>
         <Center>
           <Shirt/>
         </Center>
